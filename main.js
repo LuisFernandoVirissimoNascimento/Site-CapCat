39b59a8731d0e72710c3151a3451e9f7e8a208e0
Luis Fernando <luisvirissimo789@gmail.com>
2024-10-20 22:22:32 -0300

    Comment

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const observer = new IntersectionObserver((entries) => {
         if (entry.isIntersecting){
             entry.target.classList.add('show');
         } 
+        // Put else entry.target.classList.remove('show'); to make it so images dissapear after leaving the screen
     })
 });
 
